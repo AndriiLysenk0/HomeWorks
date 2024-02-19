dc124b72a97319df2fa657349414000f3aeeeb1f
AndriiLysenk0 <160528586+AndriiLysenk0@users.noreply.github.com>
2024-02-19 19:51:38 +0200

    feat: lesson1

---
+++ Lesson 1/main.js
@@ -0,0 +1,30 @@
+/*let a = 10;
+let b = 17;
+
+let age = a + b;
+console.log(age);
+
+let name = "Andrii";
+
+console.log(name);
+
+let surname = "Lysenko";
+
+name = name + " " + surname;
+console.log(name);*/
+
+/*alert(age);
+alert(name);*/
+
+/*const userName = prompt('Як вас звуть?');
+console.log(userName);
+const age = prompt('Ваш вік?');
+console.log(age);
+
+console.log(`вас звуть, ${userName} ваш вік, ${age}`);*/
+
+let num1 = prompt('число');
+let num2 = prompt('число');
+
+console.log(num1 + num2);
+
