ba7301b39fd17e1b5e63c8c4c080d5560318e10e
AndriiLysenk0 <160528586+AndriiLysenk0@users.noreply.github.com>
2024-02-19 20:02:34 +0200

    Update main.js

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -1,4 +1,4 @@
-/*let a = 10;
+let a = 10;
 let b = 17;
 
 let age = a + b;
@@ -11,17 +11,17 @@ console.log(name);
 let surname = "Lysenko";
 
 name = name + " " + surname;
-console.log(name);*/
+console.log(name);
 
-/*alert(age);
-alert(name);*/
+alert(age);
+alert(name);
 
-/*const userName = prompt('Як вас звуть?');
+const userName = prompt('Як вас звуть?');
 console.log(userName);
 const age = prompt('Ваш вік?');
 console.log(age);
 
-console.log(`вас звуть, ${userName} ваш вік, ${age}`);*/
+console.log(`вас звуть, ${userName} ваш вік, ${age}`);
 
 let num1 = prompt('число');
 let num2 = prompt('число');
