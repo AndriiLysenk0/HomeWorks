8954f177377aa4f05f5a5f65101e1da0e51fa39a
AndriiLysenk0 <160528586+AndriiLysenk0@users.noreply.github.com>
2024-02-22 18:58:05 +0200

    Update main.js

--- Lesson 1/main.js
+++ Lesson 1/main.js
@@ -13,8 +13,17 @@ let surname = "Lysenko";
 name = name + " " + surname;
 console.log(name);
 
-alert(age);
-alert(name);
+let name = "Andrii";
+
+let age = "27";
+
+alert('Andrii');
+alert('27');
+
+alert(`Мене звати ${name} мені ${age} років`);
+
+
+alert(мене звати 'name' мени 'age' років);
 
 const userName = prompt('Вас звати?');
 console.log(userName);
@@ -23,28 +32,17 @@ console.log(age);
 
 console.log(`Мене звуть, ${userName} мені, ${age} років`);
 
-let num1 = prompt('число');
-let num2 = prompt('число');
+let num1 = prompt('num1');
+let num2 = prompt('num2');
+num1 = Number(num1);
+num2 = Number(num2);
 
-console.log(num1 + num2);
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
 
-let num1 = prompt('число');
-let num2 = prompt('число');
-
-console.log(num1 + num2);
-
-let num1 = prompt('число');
-let num2 = prompt('число');
-
-console.log(num1 - num2);
-
-let num1 = prompt('число');
-let num2 = prompt('число');
-
-console.log(num1 * num2);
-
-let num1 = prompt('число');
-let num2 = prompt('число');
-
-console.log(num1 / num2);
-
+console.log( 'div = ', div);
+console.log( 'sub = ', sub);
+console.log( 'sum = ', sum);
+console.log( 'mul = ', mul);
