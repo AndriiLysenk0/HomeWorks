a1b57ca60fc7e946feed1955bc693a431adf45fa
AndriiLysenk0 <160528586+AndriiLysenk0@users.noreply.github.com>
2024-02-22 23:09:52 +0200

    Add files via upload

---
+++ Lesson 2/main.js
@@ -0,0 +1,45 @@
+let num1 = 5;
+let num2 = 2;
+
+prompt = (num1 + num2);
+console.log (sum = num1 + num2);
+
+console.log(5 % 2);
+console.log(5**2);
+//rem=1
+//pow=25
+console.log('Залишок від ділення ' + 5 + ' на ' + 2 + ' дорівнює ' + 1);
+console.log('Результат зведеня ' + 5 +' у ступінь ' + 2 + ' дорівнює ' + 25);
+
+
+let num = 14;
+num += 5;
+num -= 2;
+num *= 2;
+num += 3;
+console.log(num);
+//num=37
+
+
+let num = 6;
+num %= 3;
+
+let num2 = 7;
+num2 %= 3
+
+let num3 = 12;
+num3 %= 5
+
+console.log(num);
+console.log(num2);
+console.log(num3);
+
+
+let age = prompt('Ваш вік?');
+console.log(age>=18);
+
+
+let x =prompt("enter x");
+let y =prompt("enter y");
+console.log(y);
+console.log(x);
